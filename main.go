5d52453508d7760ae4410bd525da38c59f38bccd
Heilzmaker <heilzmaker.drive@gmail.com>
2025-10-19 20:17:25 +0200

    phone verification wip

--- main.go
+++ main.go
@@ -5,58 +5,62 @@ import (
 	"os"
 	"strings"
 	"bufio"
-
+	"unicode"
 )
 
 func main() {
     //firstname , lastname, email, phone number, amountoftickets into matrix
 		var firstname, lastname, email, phonenumber string
     tickets := 0
-    for i := 1; i <= 50; i = i + tickets {
-
-        for range 3 {
-            fmt.Scan(&firstname)
-            if len(firstname) > 2 {
-							break
-            }
-        }
-        fmt.Println("Type your lastname")
-        for range 3 {
-            fmt.Scan(&lastname)
-						 if len(lastname) > 2 {
-							break
-            }
-        }
-        fmt.Println("Enter your email")
-				tlds = TLD()
-        for range 3 {
-            fmt.Scan(&email)
-					validMail :=
-					if {
-						strings.Contains(email, "@")
-						
+   for i := 1; i <= 50; i = i + tickets {
+       for range 3 {
+           fmt.Scan(&firstname)
+           if len(firstname) > 2 {
+						break
+           }
+       }
+       fmt.Println("Type your lastname")
+       for range 3 {
+           fmt.Scan(&lastname)
+					 if len(lastname) > 2 {
+						break
+           }
+       }
+       fmt.Println("Enter your email")
+			tlds := TLD()
+			var validTLD bool
+       	for range 3 {
+           fmt.Scan(&email)
+				  containsAt := strings.Contains(email, "@")
+					for i := range tlds { 
+					if strings.Contains(email, tlds[i]) {
+						validTLD = true
+						break
+					} else {
+						validTLD = false
 					}
-
-        }
-        fmt.Println("Enter your phonenumber")
-        for range 3 {
-            phonenumber := fmt.scan()
-   
-        i = i + tickets
-    }
-
-    booking := [5]string{firstname, lastname, email, phonenumber, tickets}
-    i := 0
-    people := []string{}
-    people = append(people, booking[i])
+					if containsAt && validTLD {
+						break
+					}
+						
+				}
+       }
+      fmt.Println("Enter your phonenumber")
+      for range 3 {
+        fmt.Scan(&phonenumber)
+			for i := range phonenumber {
+				if !unicode.IsLetter(phonenumber) {
+					break
+				}
+			}
+		}
+   } 
 }
 
-}
+
 func TLD() []string {
 	file, err := os.Open("TLD")
-    if err != nil {
-        log.Fatalf("failed to open file: %s", err)
-    }
+  checkErr(err)
     defer file.Close() 
 
 
@@ -65,10 +69,6 @@ func TLD() []string {
 		var tlds []string
     for scanner.Scan() {
       tlds = append(tlds, scanner.Text())
-    }
-
-    if err := scanner.Err(); err != nil {
-        fmt.Printf("error reading file: %s", err)
     }
 		return tlds
 	}
